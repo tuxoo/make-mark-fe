4c2df4a2fb8fbc637fb8a5f8f8956b8df8916d75
Eugene Krivtsov <krivtsov.eugene@gmail.com>
2022-09-05 22:26:57 +0300

    MMARK: added base routing;

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import './App.css';
+import {Route, Routes} from "react-router-dom";
+import Home from "./page/Home";
+import {ToastContainer} from "react-toastify";
 
 function App() {
     return (
-        <div className="App">
-        </div>
+        <>
+            <ToastContainer/>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+            </Routes>
+        </>
     );
 }
 
